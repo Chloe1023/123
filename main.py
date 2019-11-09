018a695cf781b772e7af668e62d3ea2af1834271
Chloe1023 <chloebwoodman@gmail.com>
2019-11-08 17:21:21 -0700

    7th

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+a = [ 3, 2, 1, 4, 5 ]
+for i in range(len(a)):
+    for j in range(1, len(a)):
+        if a[j-1] > a[j]:
+            w = a[j]
+            a[j] = a[j-1]
+            a[j-1] = w
 
-print ("email", end = "@")
-print ("gmail.com", end = " ")
