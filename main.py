64580d2a8e0799ade1601f57ecfef841fe3a72ca
Chloe1023 <chloebwoodman@gmail.com>
2019-11-08 17:26:29 -0700

    9th

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ for i in range(len(a)):
 
 for i in range(len(a)):
     print(a[i], end="")
+
+a= [1, 6, 3, 9, 5]
+for i in range(len(a)-1, 0, -1):
+   for j in range(i):
+    if a[j] > a[j+1]:
+        w = a[j]
+        a[j] = a[j+1]
+        a[j+1] = w
+
+for i in range(len(a)):
+    print(a[i], end="")
